175cdd66024a478e78dee4a482221ce4979490a3
JuGeo <nahum27@naver.com>
2018-11-01 15:39:54 +0000

    web

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, request
 import random
+import requests
+from bs4 import BeautifulSoup as bssss
+import csv
+import datetime
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -86,10 +92,81 @@ def hello_man():
 @app.route("/hi")
 def hi():
     user_name = request.args.get('name')
-    return render_template('hi.html', user_name = user_name)    
+    return render_template('hi.html', user_name = user_name)
     
-    
+@app.route("/who")
+def who():
 
+    return render_template('who.html')
+
+@app.route("/who_is")
+def who_is():
+
+    li = ['컴퓨터','나무', '신', '칠판', '선생님','돌맹이','가수','번개']
+    
+    user_name = request.args.get('name')
+    user_hobby = request.args.get('hobby')
+    
+    url1 = 'https://www.google.co.kr/search?q={}&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiWtf-4ua3eAhVLO7wKHd5XDuYQ_AUIDigB&biw=1444&bih=740&dpr=1.13'.format(user_name)
+    url2 = 'https://www.google.co.kr/search?q={}&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiWtf-4ua3eAhVLO7wKHd5XDuYQ_AUIDigB&biw=1444&bih=740&dpr=1.13'.format(user_hobby)
+    
+    req = requests.get(url1).text
+    req1 = requests.get(url2).text
+    
+    name_img = bs(req, 'lxml')
+    hobby_img = bs(req1, 'lxml')
+    
+    a = name_img.findAll('img')[3]['src']
+    b = hobby_img.findAll('img')[3]['src']
+    
+    wh = random.choice(li)
+    you = '{}님 당신의 전생은 취미가 {}인 {}입니다'.format(user_name,user_hobby,wh)
+
+    
+    
+    return render_template('img.html', you = you, a = a, b = b)
+
+@app.route("/summoner")
+def summoner():
+    url = 'http://www.op.gg/summoner/userName='
+    return render_template('summoner.html')
+
+@app.route("/opgg")
+def opgg():
+    name = request.args.get('sum')
+    url = 'http://www.op.gg/summoner/userName='
+    html = requests.get(url+name).text
+    
+    soup = bssss(html, 'html.parser')
+    win = soup.select('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div.SummonerRatingMedium > div.TierRankInfo > div.TierInfo > span.WinLose > span.wins')
+    lose = soup.select('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div.SummonerRatingMedium > div.TierRankInfo > div.TierInfo > span.WinLose > span.losses')
+    #print(win)
+    
+    if len(win) == 0:
+        win = '0승'
+    else:
+        win = win[0].text
+    if len(lose) == 0:
+        lose = '0패'
+    else: 
+        lose = lose[0].text
+    # f = open("list.text", "a+")
+    # data = "소환사의 이름은 {} {}/{}입니다".format(name, win, lose)
+    # f.write(data)
+    # f.close()
+    f = open('list.csv','a+', encoding = 'utf-8', newline='')
+    csvfile = csv.writer(f)
+    data = [name, win, lose, datetime.datetime.now()]
+    csvfile.writerow(data)
+    
+    return render_template("opgg.html", name = name, win = win, lose = lose)
+
+
+@app.route('/log')
+def log():
+    f = open('list.csv', 'r', encoding = 'utf-8')
+    logs = csv.reader(f)
+    return render_template('log.html', logs = logs)
 
 
     
